7e8a34055edf2f1f0398b2a6e03e904d9c50ec56
Thales Minoru Sato <thalesms2@outlook.com>
2022-06-20 14:46:27 -0300

    feat: spinning pokeball on loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,13 +1,57 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const pokeballAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100%{
+    transform: rotate(360deg);
+  }
+`
 
 const LoadingDiv = styled.div`
-  display: flex;
+  /* display: flex;
+  height: 50vh;
   justify-content: center;
-  align-items: center;
-  h2 {
-    font-size: 4em;
-    font-family: "Flexo",arial,sans-serif;
+  align-items: center; */
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+const Pokeball = styled.div`
+  width: 60px;
+  height: 60px;
+  background-color: #fff;
+  border-radius: 50% ;
+  position: relative;
+  overflow: hidden;
+  border: 3px solid;
+  animation: ${pokeballAnimation} .8s  linear 0s infinite;
+
+  &:after{
+    content: '';
+    position: absolute;
+    width: 80px;
+    height: 32px;
+    background-color: red;
+    border-bottom: 4px solid;
+    top: -4px;
+  }
+
+  &:before{
+    content: '';
+    position: absolute;
+    background-color: #fff;
+    width: 15px;
+    height: 15px;
+    border: 4px solid;
+    border-radius: 50%;
+    bottom: 20px;
+    right: 20px;
+    z-index: 1;
   }
 `
 
@@ -15,7 +59,7 @@ const Loading: React.FC = () => {
 
     return (
         <LoadingDiv>
-            <h2>Carregando...</h2>
+            <Pokeball />
         </LoadingDiv>
     )
 }
